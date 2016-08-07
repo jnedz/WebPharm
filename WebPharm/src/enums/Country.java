d3bcf7bceca85d8d71ef0a4cd5b174c80853f406
jnedz <julia.nedzelskaya@gmail.com>
2016-08-07 23:08:04 +0300

    07.08-2

--- WebPharm/src/enums/Country.java
+++ WebPharm/src/enums/Country.java
@@ -4,5 +4,19 @@ public enum Country {
 
 	UKRAINE(1), ENGLAND(2), FRANCE(3), NOCOUNTRY (4);
 	
+	private int code;
+	private Country(int code) {
+		this.setCode(code);
+	}
+	public int getCode() {
+		return code;
+	}
+	
+	/* test */
+	
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
 	
 }
